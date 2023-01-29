be1db4300b827f421220980c31ccdabfa2c10bec
SelahattinKaanDeniz <selahattinkaandeniz@etu.edu.tr>
2023-01-29 15:21:53 +0300

    New Menu Panels added + Scoll mechanism changed to no drag

--- VRestate/Assets/Scripts/ItemMenuButtonFunctions.cs
+++ VRestate/Assets/Scripts/ItemMenuButtonFunctions.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class ItemMenuButtonFunctions : MonoBehaviour
 {
     public GameObject BathroomMenuPanel;
+    public GameObject KitchenMenuPanel;
+    public GameObject ConstructionMenuPanel;
     public GameObject ItemMenuPanel;
+
+    //Ana kategoriden Bathroom butonuna týklandýðýnda
     public void BathroomButtonClicked()
     {
         
@@ -14,10 +18,47 @@ public class ItemMenuButtonFunctions : MonoBehaviour
         ItemMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(1500f, 127f);
     }
 
+    //Ana kategoriden Kitchen butonuna týklandýðýnda
+    public void KitchenButtonClicked()
+    {
+
+        KitchenMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-660f, 127f);
+
+        ItemMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(3660f, 127f);
+    }
+
+    //Ana kategoriden Construction butonuna týklandýðýnda
+    public void ConstructionButtonClicked()
+    {
+
+        ConstructionMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-660f, 127f);
+
+        ItemMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-2820f, 127f);
+    }
+
+    //Bathroom alt kategoriden Back butonuna týklandýðýnda
     public void BathroomBackButtonClicked()
     {
        
         BathroomMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(1500f, 127f);
+
+        ItemMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-660f, 127f);
+    }
+
+    //Kitchen alt kategoriden Back butonuna týklandýðýnda
+    public void KitchenBackButtonClicked()
+    {
+
+        KitchenMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(3660f, 127f);
+
+        ItemMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-660f, 127f);
+    }
+
+    //Construction alt kategoriden Back butonuna týklandýðýnda
+    public void ConstructionBackButtonClicked()
+    {
+
+        ConstructionMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-2820f, 127f);
 
         ItemMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-660f, 127f);
     }
