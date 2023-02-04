66a803797cc9ec219e9167fe83dc58d55fa3d4de
Borahan Serel <boraserel@gmail.com>
2023-02-04 20:58:37 +0300

    login apis are added

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 let mysql = require("mysql");
+
 let connection = mysql.createConnection({
     host: 'vrestate.clbfq7mnbhip.eu-west-3.rds.amazonaws.com',
     port: '3306',
@@ -8,6 +11,8 @@ let connection = mysql.createConnection({
     database: 'vrestate'
 });
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/testapi', (req,res) => {
     let query = 'SELECT name FROM vrestate.test WHERE id = '+req.query.id;
@@ -27,6 +32,66 @@ app.get('/testapi', (req,res) => {
     });
     
     
+});
+
+app.get('/login/check', (req,res) => {
+    let query = 'SELECT * FROM vrestate.user WHERE mail = \''+req.query.mail+'\'';
+    
+    connection.query(query, (error, results, fields) => {
+        if(error){
+            res.statusMessage = 'Database Query Error: '+error;
+            res.status(500).end();
+            return console.error(error);
+        }
+        if(results.length == 0){
+            res.statusMessage = 'User Not Found';
+            res.status(400).end();
+        }
+        else{
+            res.send(results[0]);
+        }
+        
+    });
+    
+    
+});
+
+app.get('/login/getById', (req,res) => {
+    let query = 'SELECT * FROM vrestate.user WHERE id = \''+req.query.id+'\'';
+    
+    connection.query(query, (error, results, fields) => {
+        if(error){
+            res.statusMessage = 'Database Query Error: '+error;
+            res.status(500).end();
+            return console.error(error);
+        }
+        if(results.length == 0){
+            res.statusMessage = 'User Not Found';
+            res.status(400).end();
+        }
+        else{
+            res.send(results[0]);
+        }
+        
+    });
+    
+    
+});
+
+
+app.post('/login/create', (req,res) => {
+    let query = 'INSERT INTO user (name,surname,phone,mail) values (\''+req.body.name+'\',\''+req.body.surname+'\',\''+req.body.phone+'\',\''+req.body.mail+'\')';
+    connection.query(query, (error, results, fields) => {
+        if(error){
+            res.statusMessage = 'Database Query Error: '+error;
+            res.status(500).end();
+            return console.error(error);
+        }
+        else{
+            res.status(200).end();
+        }
+        
+    });
 });
 
 app.get('/loaderio-0ea84b7e34801498d9390d93deb66294', (req,res) => {
