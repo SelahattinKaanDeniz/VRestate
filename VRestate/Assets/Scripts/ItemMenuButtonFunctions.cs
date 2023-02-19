f0010f8448711b7f7e423ecb2a66e014288af583
SelahattinKaanDeniz <selahattinkaandeniz@etu.edu.tr>
2023-02-19 16:46:03 +0300

    Left Click + Right Click function added

--- VRestate/Assets/Scripts/ItemMenuButtonFunctions.cs
+++ VRestate/Assets/Scripts/ItemMenuButtonFunctions.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ItemMenuButtonFunctions : MonoBehaviour
 {
@@ -8,6 +9,16 @@ public class ItemMenuButtonFunctions : MonoBehaviour
     public GameObject KitchenMenuPanel;
     public GameObject ConstructionMenuPanel;
     public GameObject ItemMenuPanel;
+
+    public  static string clickedButtonName = "";
+    private string leftClickedButtonName;
+    [SerializeField] private Camera mainCamera;
+
+    private GameObject ObjectFollowsMouse;
+
+    public GameObject CabinetBase1;
+    private int cabinetbase1count = 0;
+    Vector3 mousepos;
 
     //Ana kategoriden Bathroom butonuna týklandýðýnda
     public void BathroomButtonClicked()
@@ -61,5 +72,94 @@ public class ItemMenuButtonFunctions : MonoBehaviour
         ConstructionMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-2820f, 127f);
 
         ItemMenuPanel.GetComponent<RectTransform>().localPosition = new Vector3(-660f, 127f);
+    }
+
+
+    //buton ismine göre bu metottan prefab oluþtur
+    public void CabinetBase1ButtonClicked() 
+    {
+        //leftClickedButtonName = EventSystem.current.currentSelectedGameObject.name;
+
+        ObjectFollowsMouse = Instantiate(CabinetBase1, new Vector3(4f, 2f, -18f), Quaternion.identity);
+        cabinetbase1count = 1;
+        
+    }
+    public void CabinetBase1ButtonRightClicked()
+    {
+        //CabinetBase1.transform.localScale = new Vector3(5f, 5f, 5f);
+        //CabinetBase1.transform.localScale = new Vector3(1f, 1f, 1f);
+        clickedButtonName = "Cabinet_Base_1";
+        Debug.Log(clickedButtonName);
+
+    }
+    public void ItemMenuButtonsRightClicked()
+    {
+        //CabinetBase1.transform.localScale = new Vector3(5f, 5f, 5f);
+        //CabinetBase1.transform.localScale = new Vector3(1f, 1f, 1f);
+        //clickedButtonName = EventSystem.current.currentSelectedGameObject.name;
+
+        PointerEventData pointer = new PointerEventData(EventSystem.current);
+        pointer.position = Input.mousePosition;
+
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointer, raycastResults);
+
+        if (raycastResults.Count > 0)
+        {
+            int indexofbutton = 0;
+            foreach (var go in raycastResults)
+            {
+                if (indexofbutton == 1)
+                {
+                     clickedButtonName = go.gameObject.name;
+                }
+                //Debug.Log(go.gameObject.name, go.gameObject);
+                indexofbutton++;
+            }
+        }
+        //clickedButtonName = "Cabinet_Base_2";
+        Debug.Log(clickedButtonName);
+
+
+
+    }
+    public void Update()
+    {
+        //SEÇÝLEN OBJE OLUÞUP MOUSEU TAKÝP EDÝYOR
+        if (cabinetbase1count == 1)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit raycastHit))
+            {
+                ObjectFollowsMouse.transform.position = raycastHit.point;
+            }
+
+            //mousepos.z = ObjectFollowsMouse.transform.position.z;
+            //ObjectFollowsMouse.transform.position = Camera.main.ScreenToWorldPoint(mousepos); 
+
+            //Rotate
+            if (MouseInputUIBlocker.BlockedByUI == false)
+            {
+                if (Input.GetAxis("Mouse ScrollWheel") > 0)
+                {
+                    ObjectFollowsMouse.transform.Rotate(0, 90f, 0, Space.Self);
+                }
+                if (Input.GetAxis("Mouse ScrollWheel") < 0)
+                {
+                    ObjectFollowsMouse.transform.Rotate(0, -90f, 0, Space.Self);
+                }
+                // Sol týk koyma
+                if (Input.GetMouseButtonDown(0))
+                {
+                    cabinetbase1count = 0;
+                }
+                // Sað týk delete
+                if (Input.GetMouseButtonDown(1))
+                {
+                    cabinetbase1count = 0;
+                    Destroy(ObjectFollowsMouse);
+                }
+            }
+        }
     }
 }
---
+++ VRestate/Assets/Scripts/SizeMenuFunctions.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SizeMenuFunctions : MonoBehaviour
+{
+    
+    private ItemMenuButtonFunctions itemMenuFunctions;
+
+    private string whichButtonClicked;
+    public TMP_InputField lengthInputField;
+    public TMP_InputField widthInputField;
+    public TMP_InputField heightInputField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject canvas = GameObject.Find("Canvas");
+        itemMenuFunctions = canvas.GetComponent<ItemMenuButtonFunctions>();
+        
+
+    }
+    public void lengthValueChanged(string txt)
+    {
+        if (txt.Length > 0 && txt[0] == '-') lengthInputField.text = txt.Remove(0, 1);
+    }
+    public void widthValueChanged(string txt)
+    {
+        if (txt.Length > 0 && txt[0] == '-') widthInputField.text = txt.Remove(0, 1);
+    }
+    public void heightValueChanged(string txt)
+    {
+        if (txt.Length > 0 && txt[0] == '-') heightInputField.text = txt.Remove(0, 1);
+    }
+
+    // OYUNDAN ÇIKIÞ YAPINCA BÜTÜN PREFABLERÝN BOYUTLARINI ESKÝ HALÝNE GETÝR
+    public void SaveButtonClicked() { 
+        float length; // X
+        float.TryParse(lengthInputField.text, out float lengthresult);
+        length = lengthresult;
+
+        float width; // Z
+        float.TryParse(widthInputField.text, out float widthresult);
+        width = widthresult;
+
+        float height; // Y
+        float.TryParse(heightInputField.text, out float heightresult);
+        height = heightresult;
+
+
+        if (length > 0 && width >0 && height > 0)
+        {
+            if(whichButtonClicked == "Cabinet_Base_1")
+            {
+                itemMenuFunctions.CabinetBase1.transform.localScale = new Vector3(length/100,height/100, width/100);
+            }
+        }
+        lengthInputField.text = "";
+        widthInputField.text = "";
+        heightInputField.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        whichButtonClicked = ItemMenuButtonFunctions.clickedButtonName;
+        
+
+    }
+}
